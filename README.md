85a3967dc461540fc743799b94277f8fc587c450
Carlos Bello <bellopauste@yahoo.com>
2014-04-07 01:08:28 +0200

    Primer commit

---
+++ README.md
@@ -0,0 +1,13 @@
+DevGym - Gimnasio para programadores
+====================================
+
+Este será un repositorio de ejercicios de programación que permita a los 
+programadores noveles adentrarse en la programación con ejercicios básicos que
+van aumentando prograsivamente en complejidad.
+
+Para aumentar el repositorio, basta con modificar el fichero exercises.json con
+la definición de los ejercicios y crear el fichero html correspondiente en la 
+carpeta exercises.
+
+La idea es que, más adelante, el programador pueda validar su ejercicio aquí 
+mismo y tener estadísticas sobre sus soluciones.
---
+++ js/main.js
@@ -0,0 +1,28 @@
+function DevGym() {
+    'use strict';
+    
+    var that = this;
+    this.currentView = ko.observable('index');
+    this.currentCategory = ko.observable();
+    this.exercises = ko.observableArray([]);
+    this.currentExcercises = ko.observableArray([]);
+
+    this.setCategory = function(category) {
+        that.currentCategory($(that.exercises()).filter(function (index, item) {
+            return item.category === category ? item : null;
+        })[0]);
+        that.currentExcercises.removeAll();
+        $.each(that.currentCategory().exercieses.sort(function (e1, e2) { return e1.complexity > e2.complexity }),
+            function (index, item) {
+                $.get('exercises/' + item.id + '.html', function (result) {
+                    that.currentExcercises.push($.extend(item, {content: result}));
+                });
+            });
+    };
+    
+    this.start = function () {
+        $.getJSON('exercises/exercises.json', function (result) {
+           that.exercises(result); 
+        });
+    };
+}
