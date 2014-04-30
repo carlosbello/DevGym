1cbf29ebb9374c3731f4dc760bb20deccb7b48b9
carlos <carlos@EliteBook>
2014-04-30 21:09:39 +0200

    fixes #2

--- exercises/iter_find-first-1_test.js
+++ exercises/iter_find-first-1_test.js
@@ -5,7 +5,7 @@
             var numberList = [2, 3, 5, 2, 8, 2, 9];
             equal(findFirst('hola', textList), -1, "No está en la lista");
             equal(findFirst('un', textList), 2, "Uno en la lista");
-            equal(findFirst(2, numberList), 3, "Varios apariciones en la lista");
+            equal(findFirst(3, numberList), 1, "Varias apariciones en la lista");
         });
     };
 })();
