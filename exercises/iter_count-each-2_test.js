4a0b7af4509c31a81621c1fcd3b89298562a17d4
carlos <carlos@EliteBook>
2014-05-20 23:01:10 +0200

    Fixes #3

--- exercises/iter_count-each-2_test.js
+++ exercises/iter_count-each-2_test.js
@@ -5,7 +5,7 @@
             var numberList = [2, 3, 5, 2, 8, 2, 9];
             deepEqual(countEach2(['hola'], textList), {'hola': 0}, "No está en la lista");
             deepEqual(countEach2(['es'], textList), {'es': 1}, "Uno en la lisa");
-            deepEqual(countEach2([2, 9], numberList), {2: 3, 0: 1}, "Varios en la lista");
+            deepEqual(countEach2([2, 9], numberList), {2: 3, 9: 1}, "Varios en la lista");
         });
     };
 })();
